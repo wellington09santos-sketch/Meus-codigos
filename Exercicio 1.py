0f1008053879370c4bb598e55ad415b6048d2d04
Wellington Santos <wellington.09santos@gmail.com>
2025-08-02 15:59:40 -0300

    Add files via upload

---
+++ Exercicio 1.py
@@ -0,0 +1,8 @@
+print('Bem vindo!\n')
+print('|'+'+ADIÇÃO ENTRE DOIS NUMEROS+'.center(78))
+print('|')
+
+num=int(input('|'.ljust(14) + 'Informe o primeiro numero para fazer o calculo:'))
+num2=int(input('|'.ljust(14) +'Informe o Segundo  numero para fazer o calculo:'))
+Total=num+num2
+print('|'.ljust(30) + f'Soma Total:{Total}')
